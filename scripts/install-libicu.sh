0cd1341cf15d44332517ed813b79744170b2939d
Dan Lindholm <git@dan.mk>
2024-12-15 23:16:19 +0100

    script to install libicu

---
+++ scripts/install-libicu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+_ICU_DIR="/tmp/build/icu"
+_ICU_SRC="https://github.com/unicode-org/icu/releases/download/release-76-1/icu4c-76_1-src.tgz"
+
+mkdir -p $_ICU_DIR
+cd $_ICU_DIR
+
+wget -q -O icu.tgz $_ICU_SRC
+gunzip -d < icu.tgz | tar xf -
+cd icu/source
+
+chmod +x runConfigureICU configure install-sh
+./runConfigureICU Linux
+gmake
+gmake install
